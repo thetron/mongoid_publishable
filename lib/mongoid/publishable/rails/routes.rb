5865dba1c06c608c09bcfbb7683369470fc4b1e6
Nicholas Bruning <nicholas@bruning.com.au>
2011-04-11 10:38:26 +1000

    Updated routes workaround

--- lib/mongoid/publishable/rails/routes.rb
+++ lib/mongoid/publishable/rails/routes.rb
@@ -1,8 +1,19 @@
 module ActionDispatch::Routing
   class Mapper
-    def publishable
-      self.get 'publish'    => 'mongoid_publishable/publish#create'
-      self.get 'unpublish'  => 'mongoid_publishable/publish#destroy'
+    def publishable(names)
+      names = Array(names)
+      if names.any?
+        get names.map{|n| n.to_s}.join('/') + "/:#{names.last.to_s.singularize}_id/publish" => 'mongoid_publishable/publish#create', :as => names.map{|n| n.to_s}.join("_") + "_publish"
+        get names.map{|n| n.to_s}.join('/') + "/:#{names.last.to_s.singularize}_id/unpublish" => 'mongoid_publishable/publish#destroy', :as => names.map{|n| n.to_s}.join("_") + "_unpublish"
+      end
+    end
+
+    # note, this is how things _should_ work (as a nested call) - but I 
+    # can't get this to properly map to the gem namespace, as rails automatically
+    # prepends the parent namespace. Left here in case a solution presents itself.
+    def is_publishable
+      get 'publish' => 'mongoid_publishable/publish#create'
+      get 'unpublish'  => 'mongoid_publishable/publish#destroy'
     end
   end
 end
